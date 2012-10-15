3420bc953ed3709c5295d666310d8f7505281697
Carlos Hugo Gonzalez Castell (fundar) <carlos@fundar.org.mx>
2012-10-15 15:46:45 -0500

    agregando footer

--- www/lib/themes/razones/footer.php
+++ www/lib/themes/razones/footer.php
@@ -4,6 +4,11 @@
 				<a href="http://fundar.org.mx" title="Fundar.org.mx">
 					<img src="<?php print $this->themePath; ?>/css/images/fundar.jpg" alt="Fundar.org.mx" />
 				</a>
+				<p class="left">
+					&copy; 2012 Fundar, Centro de An&aacute;lisis e Investigaci&oacute;n A.C.<br>
+					Tel. +52 (55) 55 54-3001 fundar@fundar.org.mx
+				</p>
+				
 			</div>
 		</div>
 	</body>
